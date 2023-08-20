c57a7455cad30e7f5689f49c806486ce6f678454
trancongtien <tientc.21it@vku.udn.vn>
2023-08-20 08:55:05 +0700

    s

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:admin_panel/constants.dart';
+import 'package:admin_panel/screens/home/main_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: bgColor,
+        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)
+            .apply(bodyColor: Colors.white),
+        canvasColor: secondaryColor,
+      ),
+      home:   MainScreen(),
+    );
+  }
+}
